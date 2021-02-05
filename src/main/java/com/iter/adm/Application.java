0542cf76d858648965f8930159f6414342794d6f
prem <prem@192.168.29.234>
2021-02-05 15:52:19 +0530

    added comment

--- src/main/java/com/iter/adm/Application.java
+++ src/main/java/com/iter/adm/Application.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	/**
+	 * This is main method of adm services
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
